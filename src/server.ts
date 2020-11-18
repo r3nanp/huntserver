d7bacbcffc38f3b4085c7923bc7a16f5950533f9
r3nanp <renanmol87@gmail.com>
2020-11-18 19:51:27 -0300

    feat (server): add error handler

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import createConnection from './database/access'
 
 import routes from './routes'
+import errorHandler from './errors/errorsHandler'
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.use(cors())
 app.use(express.json())
 createConnection()
 app.use(routes)
+
+app.use(errorHandler)
 
 app.listen(3333, () => {
   console.log('Server is running at http://localhost:3333')
