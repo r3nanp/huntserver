cf50f56c8bdf382bd73c96a51526bf551dc6e154
r3nanp <renanmol87@gmail.com>
2020-12-31 11:03:05 -0300

    feat(server): create server and import app

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,3 @@
-import express from 'express'
-import cors from 'cors'
-import createConnection from './database/access'
-
-import routes from './routes'
-import errorHandler from './errors/errorsHandler'
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-createConnection()
-app.use(routes)
-
-app.use(errorHandler)
+import { app } from './app/app'
 
 app.listen(process.env.PORT || 3333)
