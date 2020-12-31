f47e3b0c157acf576b67bdd1c454aaa71533da0b
r3nanp <renanmol87@gmail.com>
2020-12-31 10:58:34 -0300

    refactor: remove export and execute function

---
+++ src/database/createConnection.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+const uri = process.env.MONGODB_URI
+
+async function createConnection() {
+  if (!uri) {
+    return console.error('Error connecting to the database!')
+  }
+
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.info('MongoDB connect...')
+  } catch (err) {
+    console.error(`Error to connect | ${err.message}`)
+  }
+}
+
+createConnection()
