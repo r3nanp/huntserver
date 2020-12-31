87c87cfa9208413df3c960671f544d0c6247b643
r3nanp <renanmol87@gmail.com>
2020-12-31 11:01:10 -0300

    feat(ShowProductController): create product controller

---
+++ src/useCases/ShowProduct/ShowProductController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import { Product } from '../../entities/product.entity'
+
+async function ShowProductController(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const product = await Product.findById(req.params.id)
+
+  return res.json(product)
+}
+
+export { ShowProductController }
